859aaf781609b053c16711950ea327991570c4e0
mauzeroffc <mauzeroff333@gmail.com>
2023-01-29 23:52:38 +0300

    React movie app8

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Page1 from "./routers/Page_1.js";
 import Page2 from "./routers/Page_2.js";
 import Page3 from "./routers/Page_3.js";
 
 function App() {
-  return <BrowserRouter>
+  return <HashRouter>
             <Routes>
               <Route path="/" element={<Page1 />}/>
               <Route path="/1" element={<Page1 />}/>
               <Route path="/2" element={<Page2 />}/>
               <Route path="/3" element={<Page3 />}/>
             </Routes>
-  </BrowserRouter>
+  </HashRouter>
 }
 
 export default App
